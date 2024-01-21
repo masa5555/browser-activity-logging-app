7915ca01403b5b40e6d283ce5c68f0193d4df981
masa5555 <masa57887@gmail.com>
2024-01-21 15:54:53 +0900

    一旦最小限の起動まで

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { crx } from "@crxjs/vite-plugin";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import manifest from "./manifest.json";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), crx({ manifest })],
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./src/setupTests.js'],
+    environment: "jsdom",
+    setupFiles: ["./src/setupTests.js"],
+  },
+  server: {
+    port: 5174,
+    strictPort: true,
+    hmr: {
+      port: 5174,
+    },
   },
 });
